aa878de465b932eaf97f6219475c59d14e7148e9
Arshal Abbas <arshalabbas123@gmail.com>
2024-12-16 12:58:49 +0530

    Configured the adminjs

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,16 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { userRouter } from "./routes/user.route";
+
+// AdminJS Imports
+import AdminJS, { AdminJSOptions } from "adminjs";
+import AdminJSExpress from "@adminjs/express";
+import { Database, Resource, getModelByName } from "@adminjs/prisma";
+
+import prisma from "./config/prismaClient";
+
+const PORT = 3000;
+
+AdminJS.registerAdapter({ Database, Resource });
 
 const app = express();
 
@@ -12,6 +23,25 @@ app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-app.listen(3000, () => {
+// AdminJS Configurations
+const adminJsOptions: AdminJSOptions = {
+  resources: [
+    {
+      resource: { model: getModelByName("User"), client: prisma },
+      options: {},
+    },
+  ],
+};
+
+const admin = new AdminJS(adminJsOptions);
+
+const adminRouter = AdminJSExpress.buildRouter(admin);
+
+app.use(admin.options.rootPath, adminRouter);
+
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
+  console.log(
+    `AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`
+  );
 });
